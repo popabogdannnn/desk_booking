2f411483acfb7d3e1b199b70b65cef48fb81f30c
popabogdannnn <popa_bogdanioan@yahoo.com>
2022-05-27 18:05:58 +0300

    login and register working

---
+++ desk_booking/home/decorators.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+def unauthenticated_user(view_func):
+    def wraper_func(request, *args, **kwargs):
+        if (not request.user.is_authenticated):
+            return redirect('login')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wraper_func
+
+def authenticated_user(view_func):
+    def wraper_func(request, *args, **kwargs):
+        if (request.user.is_authenticated):
+            return redirect('home')
+        else:
+            return view_func(request, *args, **kwargs)
+    return wraper_func
+
